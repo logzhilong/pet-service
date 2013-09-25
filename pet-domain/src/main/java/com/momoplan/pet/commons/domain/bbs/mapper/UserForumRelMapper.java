e274af90fadaa4868aa1e35a26cef3a5de2bbc28
cc14514 <lance_fox0451@163.com>
2013-09-25 15:12:42 +0800

    修正

--- pet-domain/src/main/java/com/momoplan/pet/commons/domain/bbs/mapper/UserForumRelMapper.java
+++ pet-domain/src/main/java/com/momoplan/pet/commons/domain/bbs/mapper/UserForumRelMapper.java
@@ -11,7 +11,7 @@ import org.apache.ibatis.annotations.Param;
 * @author liangc [cc14514@icloud.com]
 * @version v1.0
 * @copy pet
-* @date 2013-09-24 11:11:36
+* @date 2013-09-25 15:10:59
 */
 public interface UserForumRelMapper {
     /**
