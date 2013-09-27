5c402215370d5e48aafda0ada8689c3915606e66
cc14514 <lance_fox0451@163.com>
2013-09-27 14:24:32 +0800

    修正

--- pet-service-bbs/src/main/java/com/momoplan/pet/framework/bbs/controller/ForumController.java
+++ pet-service-bbs/src/main/java/com/momoplan/pet/framework/bbs/controller/ForumController.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,7 +15,6 @@ import com.momoplan.pet.framework.bbs.service.NoteSubService;
 import com.momoplan.pet.framework.bbs.service.UserForumRelService;
 
 @Controller
-@TransactionConfiguration(defaultRollback=true,transactionManager="bbsTransactionManager")
 public class ForumController {
 	
 	private static Logger logger = LoggerFactory.getLogger(ForumController.class);
