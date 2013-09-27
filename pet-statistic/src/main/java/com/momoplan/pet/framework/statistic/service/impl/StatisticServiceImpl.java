db43561e1a52470a12e091361710a0bea30f405f
cc14514 <lance_fox0451@163.com>
2013-09-27 12:58:11 +0800

    jdbcTemplate demo

--- pet-statistic/src/main/java/com/momoplan/pet/framework/statistic/service/impl/StatisticServiceImpl.java
+++ pet-statistic/src/main/java/com/momoplan/pet/framework/statistic/service/impl/StatisticServiceImpl.java
@@ -1,12 +1,18 @@
 package com.momoplan.pet.framework.statistic.service.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.momoplan.pet.commons.domain.statistic.mapper.StatDeviceMapper;
 import com.momoplan.pet.commons.domain.statistic.mapper.StatHistoryLogMapper;
 import com.momoplan.pet.commons.domain.statistic.mapper.StatPetUserMapper;
@@ -19,6 +25,24 @@ public class StatisticServiceImpl implements StatisticService {
 
 	private Logger logger = LoggerFactory.getLogger(StatisticServiceImpl.class);
 
+/*
+	@Autowired
+	ComboPooledDataSource statisticDataSource = null;
+	{
+		JdbcTemplate jt = new JdbcTemplate(statisticDataSource);
+		List<StatDevice> res = jt.query("select id,name from fff ", new RowMapper<StatDevice>(){
+			@Override
+			public StatDevice mapRow(ResultSet rs, int rowNum) throws SQLException {
+				StatDevice device = new StatDevice();
+				long id = rs.getLong("id");
+				String name = rs.getString("name");
+				device.setId(id);
+				return null;
+			}
+		});
+	}
+*/
+	
 	StatDeviceMapper statDeviceMapper = null;
 	StatHistoryLogMapper statHistoryLogMapper = null;
 	StatPetUserMapper statPetUserMapper = null;
