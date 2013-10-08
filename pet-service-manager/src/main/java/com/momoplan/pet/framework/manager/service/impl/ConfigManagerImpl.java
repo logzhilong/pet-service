ee02aef24ce72a8c476a70ae7df59787e2082b65
cc14514 <lance_fox0451@163.com>
2013-10-08 15:20:38 +0800

    增加配置管理 Service

---
+++ pet-service-manager/src/main/java/com/momoplan/pet/framework/manager/service/impl/ConfigManagerImpl.java
@@ -0,0 +1,32 @@
+package com.momoplan.pet.framework.manager.service.impl;
+import java.util.Map;
+
+import com.momoplan.pet.commons.spring.Bootstrap;
+import com.momoplan.pet.commons.zoo.config.ConfigWatcher;
+import com.momoplan.pet.framework.manager.service.ConfigManager;
+
+public class ConfigManagerImpl implements ConfigManager{
+
+	private ConfigWatcher configWatcher = Bootstrap.configWatcher;
+	
+	private String buildPath(String key){
+		return configWatcher.getBasePath()+"/"+key;
+	}
+	@Override
+	public void deleteProperty(String key) throws Exception {
+		configWatcher.getStore().delete(buildPath(key),configWatcher);
+	}
+	@Override
+	public void setProperty(String key,String value) throws Exception {
+		configWatcher.getStore().write(buildPath(key), value);
+	}
+	@Override
+	public String getProperty(String key) throws Exception {
+		return configWatcher.getStore().read(buildPath(key), configWatcher);
+	}
+	@Override
+	public Map<String, String> getPublicPropertys() throws Exception {
+		return configWatcher.publicConfig;
+	}
+
+}
