e1a9470ff740f620344f109113f3fa3d13bf21e8
petnameqi <984409699@qq>
2013-10-09 11:51:31 +0800

    2013-10-09 1:增加配置信息新增功能 2:增加配置信息修改功能 3:增加配置信息删除功能

--- pet-service-manager/src/main/java/com/momoplan/pet/framework/manager/service/impl/ConfigManagerImpl.java
+++ pet-service-manager/src/main/java/com/momoplan/pet/framework/manager/service/impl/ConfigManagerImpl.java
@@ -4,31 +4,28 @@ import java.util.Map;
 import org.springframework.stereotype.Service;
 
 import com.momoplan.pet.commons.spring.Bootstrap;
-import com.momoplan.pet.commons.zoo.config.ConfigWatcher;
 import com.momoplan.pet.framework.manager.service.ConfigManager;
 @Service
 public class ConfigManagerImpl implements ConfigManager{
 
-	private ConfigWatcher configWatcher = Bootstrap.configWatcher;
-	
 	private String buildPath(String key){
-		return configWatcher.getBasePath()+"/"+key;
+		return Bootstrap.configWatcher.getBasePath()+"/"+key;
 	}
 	@Override
 	public void deleteProperty(String key) throws Exception {
-		configWatcher.getStore().delete(buildPath(key),configWatcher);
+		Bootstrap.configWatcher.getStore().delete(buildPath(key),Bootstrap.configWatcher);
 	}
 	@Override
 	public void setProperty(String key,String value) throws Exception {
-		configWatcher.getStore().write(buildPath(key), value);
+		Bootstrap.configWatcher.getStore().write(buildPath(key), value);
 	}
 	@Override
 	public String getProperty(String key) throws Exception {
-		return configWatcher.getStore().read(buildPath(key), configWatcher);
+		return Bootstrap.configWatcher.getStore().read(buildPath(key), Bootstrap.configWatcher);
 	}
 	@Override
 	public Map<String, String> getPublicPropertys() throws Exception {
-		return configWatcher.publicConfig;
+		return Bootstrap.configWatcher.publicConfig;
 	}
 
 }
