9963b16a5af530c399615fb0fb7bee53293a4402
cc14514 <lance_fox0451@163.com>
2013-10-15 11:53:37 +0800

    修正

--- pet-file-server/src/main/java/com/momoplan/pet/framework/fileserver/service/impl/FileServerImpl.java
+++ pet-file-server/src/main/java/com/momoplan/pet/framework/fileserver/service/impl/FileServerImpl.java
@@ -36,7 +36,7 @@ public class FileServerImpl implements FileServer{
 
 	private String buildFilePath(String id){
 		String basePath = commonConfig.get("uploadPath","/home/appusr/static");
-		SimpleDateFormat format = new SimpleDateFormat("yyyymmdd");
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/mm/dd");
 		String today = format.format(new Date());
 		File realPath = new File(basePath+"/"+today);
 		if(!(realPath.exists()&&realPath.isDirectory())){
