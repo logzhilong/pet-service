5d15498a28deb9bd117f52e9b3c2aeeb40182f52
anton465069121 <anton465069121@hotmail.com>
2013-10-21 19:01:11 +0800

    修改距离展示

--- pet/src/main/java/com/momoplan/pet/vo/PetUserView.java
+++ pet/src/main/java/com/momoplan/pet/vo/PetUserView.java
@@ -168,13 +168,13 @@ public class PetUserView implements Comparable<PetUserView>{
     public int compareTo(PetUserView arg0) {
 		int thisDis = 0;
 		int argDis = 0;
-		if(this.getDistance().contains("千米以内")){
-			thisDis = (int) (Double.parseDouble(this.getDistance().substring(0, this.getDistance().length()-4))*1000);
+		if(this.getDistance().contains("m")){
+			thisDis = (int) (Double.parseDouble(this.getDistance().substring(0, this.getDistance().length()-1))*1000);
 		}else{
 			thisDis = Integer.parseInt(this.getDistance().substring(0, this.getDistance().length()-3));
 		}
-		if(arg0.getDistance().contains("千米以内")){
-			argDis = (int) (Double.parseDouble(arg0.getDistance().substring(0, arg0.getDistance().length()-4))*1000);
+		if(arg0.getDistance().contains("km")){
+			argDis = (int) (Double.parseDouble(arg0.getDistance().substring(0, arg0.getDistance().length()-2))*1000);
 		}else{
 			argDis = Integer.parseInt(arg0.getDistance().substring(0, arg0.getDistance().length()-3));
 		}
