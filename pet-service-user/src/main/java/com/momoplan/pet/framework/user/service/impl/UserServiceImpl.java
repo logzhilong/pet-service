2fff5b810cfe2b7ec8cf50c25106b97a517af9ec
cc14514 <lance_fox0451@163.com>
2013-10-22 15:22:35 +0800

    修正

--- pet-service-user/src/main/java/com/momoplan/pet/framework/user/service/impl/UserServiceImpl.java
+++ pet-service-user/src/main/java/com/momoplan/pet/framework/user/service/impl/UserServiceImpl.java
@@ -10,13 +10,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import redis.clients.jedis.ShardedJedis;
+
+import com.momoplan.pet.commons.MyGson;
 import com.momoplan.pet.commons.cache.MapperOnCache;
+import com.momoplan.pet.commons.cache.pool.RedisPool;
+import com.momoplan.pet.commons.domain.ssoserver.dto.UserLocation;
 import com.momoplan.pet.commons.domain.ssoserver.mapper.PetInfoMapper;
 import com.momoplan.pet.commons.domain.ssoserver.po.PetInfo;
 import com.momoplan.pet.commons.domain.ssoserver.po.PetInfoCriteria;
 import com.momoplan.pet.commons.domain.ssoserver.po.SsoAuthenticationToken;
 import com.momoplan.pet.commons.domain.ssoserver.po.SsoUser;
-import com.momoplan.pet.commons.domain.ssoserver.po.UserLocation;
 import com.momoplan.pet.framework.user.service.UserService;
 import com.momoplan.pet.framework.user.vo.UserVo;
 /**
@@ -32,6 +36,8 @@ public class UserServiceImpl implements UserService {
 	private MapperOnCache mapperOnCache = null;
 	@Autowired
 	private PetInfoMapper petInfoMapper = null; 
+	@Autowired
+	private RedisPool redisPool = null;
 	
 	@Override
 	public void updateUser(SsoUser user) throws Exception {
@@ -40,28 +46,44 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public void updateUserLocation(String userId, double longitude, double latitude) throws Exception {
-		Long uid = Long.parseLong(userId);
-		UserLocation ul = mapperOnCache.selectByPrimaryKey(UserLocation.class, uid);
-		if(ul == null){
-			ul = new UserLocation();
-			ul.setUserid(uid);
+		ShardedJedis jedis = null;
+		try{
+			jedis = redisPool.getConn();
+			UserLocation ul = new UserLocation();
 			ul.setCreateDate(new Date());
 			ul.setLatitude(latitude);
 			ul.setLongitude(longitude);
-			mapperOnCache.insertSelective(ul, uid);
-		}else{
-			ul.setCreateDate(new Date());
-			ul.setLatitude(latitude);
-			ul.setLongitude(longitude);
-			mapperOnCache.updateByPrimaryKeySelective(ul, uid);
+			ul.setUserid(userId);
+			String json = MyGson.getInstance().toJson(ul);
+			logger.debug("更新坐标 : "+json);
+			jedis.lpush(LIST_USER_LOCATION+userId, json);
+		}catch(Exception e){
+			logger.error("更新坐标异常",e);
+		}finally{
+			redisPool.closeConn(jedis);
 		}
 	}
-	
+	private UserLocation getUserLocation(String userId){
+		ShardedJedis jedis = null;
+		try{
+			jedis = redisPool.getConn();
+			List<String> list = jedis.lrange(LIST_USER_LOCATION+userId, 0, 0);
+			String json = list.get(0);
+			logger.debug("获取坐标 : "+json);
+			UserLocation ul = MyGson.getInstance().fromJson(json, UserLocation.class);
+			return ul;
+		}catch(Exception e){
+			logger.error("获取坐标异常",e);
+		}finally{
+			redisPool.closeConn(jedis);
+		}
+		return null;
+	}
 	@Override
 	public UserVo getUser(SsoAuthenticationToken tokenObj) throws Exception {
 		Long userid = tokenObj.getUserid();
 		SsoUser user = mapperOnCache.selectByPrimaryKey(SsoUser.class, userid);
-		UserLocation userLocation = mapperOnCache.selectByPrimaryKey(UserLocation.class, userid);
+		UserLocation userLocation = getUserLocation(userid+"");
 		UserVo userVo = null;
 		user.setPassword(null);
 		if(userLocation!=null){
