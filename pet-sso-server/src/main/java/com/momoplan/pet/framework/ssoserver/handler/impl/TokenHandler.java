d8b0ea463b8d081182a6f80eb81dc3814a7bdba1
anton465069121 <anton465069121@hotmail.com>
2013-10-22 10:05:20 +0800

    修改token等路

--- pet-sso-server/src/main/java/com/momoplan/pet/framework/ssoserver/handler/impl/TokenHandler.java
+++ pet-sso-server/src/main/java/com/momoplan/pet/framework/ssoserver/handler/impl/TokenHandler.java
@@ -25,7 +25,7 @@ public class TokenHandler extends AbstractHandler {
 	public void process(ClientRequest clientRequest, HttpServletResponse response) throws Exception {
 		String rtn = null;
 		try{
-			String _token = PetUtil.getParameter(clientRequest, "token");
+			String _token = clientRequest.getToken();
 			LoginResponse loginResponse = ssoService.getToken(_token);
 			logger.debug("token有效 body="+_token);
 			rtn = new Success(true,loginResponse).toString();
