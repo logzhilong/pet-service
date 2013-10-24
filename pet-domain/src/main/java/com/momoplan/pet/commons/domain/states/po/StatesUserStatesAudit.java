0b94aa12156f54ec69506d4a67d9857c2fa42b0b
cc14514 <lance_fox0451@163.com>
2013-10-24 15:36:44 +0800

    修正

--- pet-domain/src/main/java/com/momoplan/pet/commons/domain/states/po/StatesUserStatesAudit.java
+++ pet-domain/src/main/java/com/momoplan/pet/commons/domain/states/po/StatesUserStatesAudit.java
@@ -11,7 +11,7 @@ import org.apache.commons.lang.builder.ToStringStyle;
 * @author liangc [cc14514@icloud.com]
 * @version v1.0
 * @copy pet
-* @date 2013-10-21 17:03:14
+* @date 2013-10-24 15:35:10
 */
 public class StatesUserStatesAudit implements Serializable {
     private static final long serialVersionUID = 1L;
--- pet-domain/src/test/java/com/momoplan/pet/domain/test/StatesTest.java
+++ pet-domain/src/test/java/com/momoplan/pet/domain/test/StatesTest.java
@@ -1,11 +1,15 @@
 package com.momoplan.pet.domain.test;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
 
 import com.momoplan.pet.AbstractTest;
+import com.momoplan.pet.commons.IDCreater;
 import com.momoplan.pet.commons.domain.states.mapper.StatesUserStatesMapper;
+import com.momoplan.pet.commons.domain.states.po.StatesUserStates;
 
 public class StatesTest extends AbstractTest {
 
@@ -15,6 +19,15 @@ public class StatesTest extends AbstractTest {
 	
 	@Test
 	public void test(){
+		StatesUserStates statesUserStates = new StatesUserStates();
+		statesUserStates.setId(IDCreater.uuid());
+		statesUserStates.setCt(new Date());
+		statesUserStatesMapper.insertSelective(statesUserStates);
+		System.out.println("OK.........");
+		System.out.println("OK.........");
+		System.out.println("OK.........");
+		System.out.println("OK.........");
+		System.out.println("OK.........");
 	}
 	
 }
