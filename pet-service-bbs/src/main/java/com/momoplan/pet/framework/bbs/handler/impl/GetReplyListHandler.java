5639be967be074f3d18cd6147a08767a8a076f70
cc14514 <cc14514@github.com>
2013-11-05 17:20:52 +0800

    修正...

--- pet-service-bbs/src/main/java/com/momoplan/pet/framework/bbs/handler/impl/GetReplyListHandler.java
+++ pet-service-bbs/src/main/java/com/momoplan/pet/framework/bbs/handler/impl/GetReplyListHandler.java
@@ -1,11 +1,18 @@
 package com.momoplan.pet.framework.bbs.handler.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.momoplan.pet.commons.MyGson;
 import com.momoplan.pet.commons.PetUtil;
 import com.momoplan.pet.commons.bean.ClientRequest;
 import com.momoplan.pet.commons.bean.Success;
@@ -20,7 +27,17 @@ import com.momoplan.pet.framework.bbs.vo.PageBean;
 public class GetReplyListHandler extends AbstractHandler {
 
 	private Logger logger = LoggerFactory.getLogger(GetReplyListHandler.class);
-
+	public static void main(String[] args) throws JSONException {
+		PageBean<NoteSubVo> o = new PageBean<NoteSubVo>();
+		List<NoteSubVo> l = new ArrayList<NoteSubVo>();
+		NoteSubVo vo = new NoteSubVo();
+		vo.setId("aa");
+		l.add(vo);
+//		o.setData(l);
+		String j = MyGson.getInstance().toJson(o.getData());
+		JSONArray arr = new JSONArray(j);
+		System.out.println(arr);
+	}
 	@Override
 	public void process(ClientRequest clientRequest, HttpServletResponse response) throws Exception {
 		String rtn = null;
@@ -30,8 +47,21 @@ public class GetReplyListHandler extends AbstractHandler {
 			String noteid = PetUtil.getParameter(clientRequest, "noteId");
 			String userId = PetUtil.getParameter(clientRequest, "userId");
 			PageBean<NoteSubVo> vo = noteSubService.getReplyByNoteId(noteid,userId, pageNo, pageSize);
+			String jsonArr = null;
+			if(vo.getData()!=null&&vo.getData().size()>0)
+				jsonArr = gson.toJson(vo.getData());
+			JSONObject success = new JSONObject();
+			success.put("success", true);
+			JSONObject entity = new JSONObject();
+			entity.put("pageSize", pageSize);
+			entity.put("pageNo", pageNo);
+			entity.put("totalCount", vo.getTotalCount());
+			if(jsonArr!=null){
+				entity.put("data", new JSONArray(jsonArr));
+			}
+			success.put("entity", entity);
 			logger.debug("根据帖子id获取所有回复成功 body=" + gson.toJson(clientRequest));
-			rtn = new Success(true, vo).toString();
+			rtn = success.toString();
 		} catch (Exception e) {
 			logger.error("根据帖子id获取所有回复失败 body=" + gson.toJson(clientRequest));
 			logger.error("login : ", e);
