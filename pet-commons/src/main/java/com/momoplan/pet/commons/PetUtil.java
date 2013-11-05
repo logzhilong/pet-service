ce57367c3eaaf13f5bd5c23d27202754aa230a90
cc14514 <cc14514@github.com>
2013-11-05 15:17:32 +0800

    修正 petutil

--- pet-commons/src/main/java/com/momoplan/pet/commons/PetUtil.java
+++ pet-commons/src/main/java/com/momoplan/pet/commons/PetUtil.java
@@ -23,7 +23,7 @@ public class PetUtil {
 	
 	public static String getParameter(PetRequest request, String key) {
 		Object value = request.getParams().get(key);
-		return value==null?null:value.toString();
+		return value!=null&&!"".equals(value)?value.toString():null;
 	}
 	
 	public static long getParameterLong(PetRequest request, String key) {
