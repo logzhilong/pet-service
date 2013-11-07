97fa1332c669bfb7e22ce9e1c62f93c0b3acc42f
petnameqi <984409699@qq>
2013-11-07 10:00:46 +0800

    2013-11-7

--- pet-service-manager/src/main/java/com/momoplan/pet/framework/manager/controller/BBSManagerController.java
+++ pet-service-manager/src/main/java/com/momoplan/pet/framework/manager/controller/BBSManagerController.java
@@ -1,22 +1,11 @@
 package com.momoplan.pet.framework.manager.controller;
 
-import java.io.File;
 import java.io.PrintWriter;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,9 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.momoplan.pet.commons.domain.bbs.po.CommonAreaCode;
 import com.momoplan.pet.commons.domain.bbs.po.Forum;
@@ -34,6 +20,7 @@ import com.momoplan.pet.commons.domain.bbs.po.Note;
 import com.momoplan.pet.framework.manager.service.BBSManagerService;
 import com.momoplan.pet.framework.manager.service.CommonDataManagerService;
 import com.momoplan.pet.framework.manager.vo.PageBean;
+import com.momoplan.pet.framework.manager.vo.UpImgVo;
 
 @Controller
 public class BBSManagerController {
@@ -50,20 +37,22 @@ public class BBSManagerController {
 	 * 
 	 * @param forum
 	 * @param model
-	 * @return 
+	 * @return
 	 */
 	@RequestMapping("/manager/bbs/ToaddOrUpdateForum.html")
 	public String ToaddOrEditAreaCode(Forum forum, Model model) {
 		try {
 			if ("".equals(forum.getId()) || null == forum.getId()) {
-				List<CommonAreaCode> codes = commonDataManagerService.getConmonArealist();
+				List<CommonAreaCode> codes = commonDataManagerService
+						.getConmonArealist();
 				model.addAttribute("codes", codes);
 				List<Forum> forums = bBSManagerService.getForumlist();
 				model.addAttribute("forums", forums);
 				logger.debug("wlcome to pet manager Forumadd......");
 				return "/manager/bbs/forumAdd";
 			} else {
-				List<CommonAreaCode> codes = commonDataManagerService.getConmonArealist();
+				List<CommonAreaCode> codes = commonDataManagerService
+						.getConmonArealist();
 				model.addAttribute("codes", codes);
 				Forum fos = bBSManagerService.getForumbyid(forum);
 				model.addAttribute("fos", fos);
@@ -71,7 +60,7 @@ public class BBSManagerController {
 				return "/manager/bbs/forumUpdate";
 			}
 		} catch (Exception e) {
-			logger.error("ToaddOrEditAreaCode"+e);
+			logger.error("ToaddOrEditAreaCode" + e);
 			e.printStackTrace();
 			return null;
 		}
@@ -97,7 +86,7 @@ public class BBSManagerController {
 		json.put("forwardUrl", "");
 		json.put("navTabId", "panel0002");
 		try {
-			//看地区是否有选择,若有,则作为条件
+			// 看地区是否有选择,若有,则作为条件
 			if (!"all".equals(fatherid) && !"all".equals(sunid)
 					&& !"all".equals(grandsunid) && !"".equals(fatherid)) {
 
@@ -109,9 +98,9 @@ public class BBSManagerController {
 					forum.setAreaCode(fatherid);
 				}
 			}
-			bBSManagerService.addOrUpdateForum(forum,request);
+			bBSManagerService.addOrUpdateForum(forum, request);
 		} catch (Exception e) {
-			logger.error("addOrUpdateForum"+e);
+			logger.error("addOrUpdateForum" + e);
 			json.put("message", e.getMessage());
 			e.printStackTrace();
 		}
@@ -143,7 +132,7 @@ public class BBSManagerController {
 		try {
 			bBSManagerService.DelForum(forum);
 		} catch (Exception e) {
-			logger.error("DelForum"+e);
+			logger.error("DelForum" + e);
 			json.put("message", e.getMessage());
 			e.printStackTrace();
 		}
@@ -156,7 +145,7 @@ public class BBSManagerController {
 	@RequestMapping("/manager/bbs/main.html")
 	public String main(Model model, HttpServletRequest request,
 			HttpServletResponse response) {
-		    logger.debug("wlcome to pet-service-bbs manager main ......");
+		logger.debug("wlcome to pet-service-bbs manager main ......");
 		return "/manager/bbs/main";
 	}
 
@@ -175,10 +164,11 @@ public class BBSManagerController {
 			pageBean = bBSManagerService.listForum(pageBean, myForm);
 			model.addAttribute("pageBean", pageBean);
 			// 读取所有国家(查询级联)
-			List<CommonAreaCode> codes = commonDataManagerService.getConmonArealist();
+			List<CommonAreaCode> codes = commonDataManagerService
+					.getConmonArealist();
 			model.addAttribute("codes", codes);
 		} catch (Exception e) {
-			logger.error("forumList"+e);
+			logger.error("forumList" + e);
 			logger.error(e.getMessage());
 		}
 		return "/manager/bbs/forumList";
@@ -202,7 +192,7 @@ public class BBSManagerController {
 			pageBean.setPageSize(100);
 			// 测试时先写死，分页尚未实现 <<<<<<<<<<
 		} catch (Exception e) {
-			logger.error("forumManager"+e);
+			logger.error("forumManager" + e);
 			logger.error(e.getMessage());
 		}
 		return "/manager/bbs/forumManager";
@@ -231,14 +221,14 @@ public class BBSManagerController {
 				return fmRight(id, model);
 			}
 		} catch (Exception e) {
-			logger.error("forumManagerMain"+e);
+			logger.error("forumManagerMain" + e);
 			e.printStackTrace();
 			return null;
 		}
 	}
 
 	private String fmLeft(String id, Model model) {
-			return "/manager/bbs/forumManagerLeft";
+		return "/manager/bbs/forumManagerLeft";
 	}
 
 	public String fmRight(String id, Model model) {
@@ -246,25 +236,24 @@ public class BBSManagerController {
 	}
 
 	/**
-	 * 获取某圈子子集贴子
-	 * 管理右侧的帖子集合
+	 * 获取某圈子子集贴子 管理右侧的帖子集合
 	 * 
 	 * @param id
 	 * @param model
 	 * @return
 	 */
 	@RequestMapping("/manager/bbs/forumrightmanagelist.html")
-	public String forumrightmanagelist(String tname,Forum forum, Model model) {
+	public String forumrightmanagelist(String tname, Forum forum, Model model) {
 		try {
 			forum.setDescript(tname);
 			List<Note> forums = bBSManagerService.getAllNotesByForumId(forum);
 			logger.debug("forumrightmanagelist" + forums.toString());
 			model.addAttribute("forums", forums);
 			model.addAttribute("forumid", forum.getId());
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-			logger.error("forumrightmanagelistError"+e);
+			logger.error("forumrightmanagelistError" + e);
 			return null;
 		}
 		return "/manager/bbs/forumManagerRight";
@@ -292,96 +281,120 @@ public class BBSManagerController {
 	 * 
 	 * @return model
 	 */
-	@SuppressWarnings("deprecation")
+	// @RequestMapping("/manager/notemanage/upimg.html")
+	// public void upImg(Model model, HttpServletRequest req,HttpServletResponse
+	// response) throws Exception {
+	// logger.debug("wlcome to upimg ......");
+	// try {
+	// CommonsMultipartResolver multipartResolver = new
+	// CommonsMultipartResolver(req.getSession().getServletContext());
+	// // 用于显示在在线编辑器里，图片的路径
+	// String newFileName = null;
+	// if (multipartResolver.isMultipart(req)) {
+	// MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest)
+	// req;
+	// Iterator<String> iter = multiRequest.getFileNames();
+	// while (iter.hasNext()) {
+	// // 上传文件信息
+	// MultipartFile file = multiRequest.getFile(iter.next());
+	// if (file != null) {
+	// String name = file.getOriginalFilename();
+	// String path = "D:\\uploadimg\\" + name;
+	// // 将上传文件存储至本地
+	// File localFile = new File(path);
+	// if (!localFile.exists()) {
+	// localFile.mkdirs();
+	// }
+	// file.transferTo(localFile);
+	// DefaultHttpClient httpclient = new DefaultHttpClient();
+	// // 请求处理页面
+	// HttpPost httppost = new
+	// HttpPost("http://123.178.27.74/pet-file-server/put");
+	// // 创建待处理的文件
+	// FileBody file1 = new FileBody(new File(path));
+	//
+	// // 对请求的表单域进行填充
+	// MultipartEntity reqEntity = new MultipartEntity();
+	// reqEntity.addPart("file", file1);
+	// reqEntity.addPart("fileName", new StringBody(name));
+	// //TODO暂时先放一个能通过token
+	// reqEntity.addPart("token", new
+	// StringBody("694359BE12E04E0088B78F297CDD3F61"));
+	// reqEntity.addPart("mimeType", new StringBody("image/jpeg"));
+	// // 设置请求
+	// httppost.setEntity(reqEntity);
+	// // 执行
+	// HttpResponse response1 = httpclient.execute(httppost);
+	// String output=null;
+	// if (HttpStatus.SC_OK == response1.getStatusLine().getStatusCode()) {
+	// logger.debug("连接通过!");
+	// HttpEntity entity = response1.getEntity();
+	// // 显示内容
+	// if (entity != null) {
+	// output=EntityUtils.toString(entity);
+	// JSONObject jsonObj = new JSONObject(output);
+	// String success = jsonObj.getString("success");
+	// if(success == "true"){
+	// String enty = jsonObj.getString("entity");
+	// newFileName ="http://123.178.27.74/pet-file-server/get/"+enty;
+	// }else{
+	// newFileName="http://123.178.27.74/pet-file-server/get/4F20CD8AAEB34C87A733657543863159";
+	// }
+	// }
+	// if (entity != null) {
+	// entity.consumeContent();
+	// }
+	// }
+	// logger.debug("返回值为:"+output);
+	// PrintWriter out = response.getWriter();
+	// out.println("{\"err\":\"" + "" + "\",\"msg\":\""+newFileName + "\"}");
+	// out.flush();
+	// out.close();
+	// }
+	// }
+	// }
+	// logger.debug("上传成功!");
+	// } catch (Exception e) {
+	// logger.error("上传异常:"+e);
+	// e.printStackTrace();
+	// PrintWriter out = response.getWriter();
+	// out.println("{\"err\":\"" + "error" + "\",\"msg\":\"" + "上传错误!......" +
+	// "\"}");
+	// out.flush();
+	// out.close();
+	// }
+	// }
 	@RequestMapping("/manager/notemanage/upimg.html")
 	public void upImg(Model model, HttpServletRequest req,HttpServletResponse response) throws Exception {
 		logger.debug("wlcome to upimg ......");
+		PrintWriter out = response.getWriter();
 		try {
-			CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(req.getSession().getServletContext());
 			// 用于显示在在线编辑器里，图片的路径
 			String newFileName = null;
-			if (multipartResolver.isMultipart(req)) {
-				MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) req;
-				Iterator<String> iter = multiRequest.getFileNames();
-				while (iter.hasNext()) {
-					// 上传文件信息
-					MultipartFile file = multiRequest.getFile(iter.next());
-					if (file != null) {
-						String name = file.getOriginalFilename();
-						String path = "D:\\uploadimg\\" + name;
-						// 将上传文件存储至本地
-						File localFile = new File(path);
-						if (!localFile.exists()) {
-							localFile.mkdirs();
-						}
-						file.transferTo(localFile);
-						DefaultHttpClient httpclient = new DefaultHttpClient();
-						// 请求处理页面
-						HttpPost httppost = new HttpPost("http://123.178.27.74/pet-file-server/put");
-						// 创建待处理的文件
-						FileBody file1 = new FileBody(new File(path));
-
-						// 对请求的表单域进行填充
-						MultipartEntity reqEntity = new MultipartEntity();
-						reqEntity.addPart("file", file1);
-						reqEntity.addPart("fileName", new StringBody(name));
-						//TODO暂时先放一个能通过token
-						reqEntity.addPart("token", new StringBody("694359BE12E04E0088B78F297CDD3F61"));
-						reqEntity.addPart("mimeType", new StringBody("image/jpeg"));
-						// 设置请求
-						httppost.setEntity(reqEntity);
-						// 执行
-						HttpResponse response1 = httpclient.execute(httppost);
-						String output=null;
-						if (HttpStatus.SC_OK == response1.getStatusLine().getStatusCode()) {
-							logger.debug("连接通过!");
-							HttpEntity entity = response1.getEntity();
-							// 显示内容
-							if (entity != null) {
-								output=EntityUtils.toString(entity);
-								JSONObject jsonObj = new JSONObject(output);  
-								String success = jsonObj.getString("success");  
-								if(success == "true"){
-									String enty = jsonObj.getString("entity"); 
-									newFileName ="http://123.178.27.74/pet-file-server/get/"+enty;
-								}else{
-									newFileName="http://123.178.27.74/pet-file-server/get/4F20CD8AAEB34C87A733657543863159";
-								}
-							}
-							if (entity != null) {
-								entity.consumeContent();
-							}
-						}
-						logger.debug("返回值为:"+output);
-						PrintWriter out = response.getWriter();
-						out.println("{\"err\":\"" + "" + "\",\"msg\":\""+newFileName + "\"}");
-						out.flush();
-						out.close();
-					}
-				}
+			UpImgVo imgVo=new UpImgVo();
+			String enty=imgVo.upimg(req);
+			if(enty != null && "" != enty){
+				newFileName = "http://123.178.27.74/pet-file-server/get/" + enty;
+			out.println("{\"err\":\"" + "" + "\",\"msg\":\"" + newFileName+ "\"}");
 			}
-			logger.debug("上传成功!");
 		} catch (Exception e) {
-			logger.error("上传异常:"+e);
 			e.printStackTrace();
-			PrintWriter out = response.getWriter();
-			out.println("{\"err\":\"" + "error" + "\",\"msg\":\"" + "上传错误!......" + "\"}");
+			out.println("{\"err\":\"" + "error" + "\",\"msg\":\""+ "上传错误!......" + "\"}");
+		}
+		finally{
 			out.flush();
 			out.close();
 		}
 	}
-	
-	
-	
 	@RequestMapping("/manager/forummamage/Toupdateforum.html")
-	public String Toupdateforum(Forum forum,Model model){
+	public String Toupdateforum(Forum forum, Model model) {
 		try {
-			forum=bBSManagerService.getForumbyid(forum);
+			forum = bBSManagerService.getForumbyid(forum);
 			model.addAttribute("fos", forum);
 			return "/manager/bbs/forumUpdate";
 		} catch (Exception e) {
-			logger.error("Toupdateforum"+e);
-			return  null;
+			logger.error("Toupdateforum" + e);
+			return null;
 		}
 	}
 }
---
+++ pet-service-manager/src/main/java/com/momoplan/pet/framework/manager/vo/UpImgVo.java
@@ -0,0 +1,92 @@
+package com.momoplan.pet.framework.manager.vo;
+
+import java.io.File;
+import java.util.Iterator;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+public class UpImgVo {
+	private Logger logger = LoggerFactory.getLogger(UpImgVo.class);
+	@SuppressWarnings("deprecation")
+	public String upimg(HttpServletRequest req){
+		try {
+			CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(req.getSession().getServletContext());
+			String enty =null;
+			if (multipartResolver.isMultipart(req)) {
+				MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) req;
+				Iterator<String> iter = multiRequest.getFileNames();
+				while (iter.hasNext()) {
+					// 上传文件信息
+					MultipartFile file = multiRequest.getFile(iter.next());
+					if (file != null) {
+						String name = file.getOriginalFilename();
+						String path = "D:\\uploadimg\\" + name;
+						// 将上传文件存储至本地
+						File localFile = new File(path);
+						if (!localFile.exists()) {
+							localFile.mkdirs();
+						}
+						file.transferTo(localFile);
+						DefaultHttpClient httpclient = new DefaultHttpClient();
+						// 请求处理页面
+						HttpPost httppost = new HttpPost("http://123.178.27.74/pet-file-server/put");
+						// 创建待处理的文件
+						FileBody file1 = new FileBody(new File(path));
+						// 对请求的表单域进行填充
+						MultipartEntity reqEntity = new MultipartEntity();
+						reqEntity.addPart("file", file1);
+						reqEntity.addPart("fileName", new StringBody(name));
+						//TODO暂时先放一个能通过token
+						reqEntity.addPart("token", new StringBody("694359BE12E04E0088B78F297CDD3F61"));
+						reqEntity.addPart("mimeType", new StringBody("image/jpeg"));
+						// 设置请求
+						httppost.setEntity(reqEntity);
+						// 执行
+						HttpResponse response1 = httpclient.execute(httppost);
+						String output=null;
+						if (HttpStatus.SC_OK == response1.getStatusLine().getStatusCode()) {
+							logger.debug("连接通过!");
+							HttpEntity entity = response1.getEntity();
+							// 显示内容
+							if (entity != null) {
+								output=EntityUtils.toString(entity);
+								JSONObject jsonObj = new JSONObject(output);  
+								String success = jsonObj.getString("success");  
+								if(success == "true"){
+									enty = jsonObj.getString("entity"); 
+								}
+							}
+							if (entity != null) {
+								entity.consumeContent();
+							}
+						}
+					}
+				}
+			}
+			logger.debug("上传成功!");
+			logger.debug("返回图片id:"+enty);
+			return enty;
+		} catch (Exception e) {
+			logger.error("上传异常:"+e);
+			return null;
+		}
+		
+	}
+	
+}
