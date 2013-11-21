68175fd711180eb3eb72f7c3caa3e29a277b5c3d
cc14514 <cc14514@github.com>
2013-11-21 17:22:36 +0800

    修正统计脚本

--- utils/access_log_analysis.py
+++ utils/access_log_analysis.py
@@ -8,9 +8,13 @@ import json
 import mod_conf
 import mod_log as lm
 import mysql.connector
+import datetime
+import redis
 
-log={}
-common_cfg={}
+
+common_cfg = mod_conf.load('access_log_analysis.ini')
+LOG_LEVEL = lm.level['debug']
+log = lm.LoggerFactory(common_cfg['common']['log_file'],'counter',LOG_LEVEL).getLog()
 
 total = {}
 result = {}
@@ -43,7 +47,7 @@ def analysis(path) :
 					c = 1
 				else:
 					c+=1
-				if service!=None :
+				if service :
 					result[k] = c	
 				line = f.readline()
 				t+=1
@@ -66,6 +70,9 @@ class Datasource:
 	def getConnection(self):
 		return mysql.connector.connect(user=self.user,password=self.pwd,host=self.host,database=self.db)
 
+		
+
+
 def runner() :
 	os.path.walk(common_cfg['common']['data_dir'],visit,'pet_access')
 	count = 0
@@ -80,37 +87,94 @@ def runner() :
 	user = common_cfg['common']['user']
 	pwd = common_cfg['common']['password']
 	db = common_cfg['common']['database']
-	
 	conn = Datasource(host,user,pwd,db).getConnection()
 	cursor = conn.cursor()
 
 	keys = result.keys()
 	keys.sort()
 	sql_insert = common_cfg['common']['sql_insert']
+	#print 'result >>>>>>>>>>>'
+	#print result
+	#print 'result <<<<<<<<<<<'
 	for k in keys:
-		rl = list(k)
-		rl.append(result.get(k))
-		t = tuple(rl)
 		template = string.Template(sql_insert)
-		#service,method,channel,d,map.get('output').get('success')
-		#('${id}','${service}','${method}','${channel}','${cd}','${success}','${counter}')			
+		#key 的结构固定为 ： service,method,channel,d,map.get('output').get('success')
+		#sql参数 ('${id}','${service}','${method}','${channel}','${cd}','${success}','${counter}')			
 		param = dict(zip(('service','method','channel','cd','success'),k))
 		param['id'] = uuid.uuid1().hex
 		param['counter'] = result.get(k)
 		sql = template.safe_substitute(param)
+		#print "k >>>>>>>>>>>>>>"
+		#print k
+		#print param
+		#print "k <<<<<<<<<<<<<<"
 		cursor.execute(sql)
-
 	cursor.close()
 	conn.commit()
 	conn.close()
 	log.debug( 'insert_rows=%s ; pv=%s' % ( len(result),count ) )
 	total.clear()
 	result.clear()	
+	#如异常，重试3次
+	for i in range(0,3):
+		try:
+			report_channel_counter()
+			break;
+		except:
+			error = traceback.format_exc()		
+			log.debug( (">>>>>>>>>>>>>> %s" % i) )
+			log.debug(error)
+			log.debug( ("<<<<<<<<<<<<<< %s" % i) )
+	return 'success'
+
+def report_channel_counter():
+	d = datetime.date.today()+datetime.timedelta(-1)
+	cd = d.strftime('%Y-%m-%d')
+	query_param = {'cd':cd}	
+	template = string.Template(report_channel_counter_sql)
+	sql = template.safe_substitute(query_param)
+	#log.debug(sql)
+	host = common_cfg['common']['host']
+	user = common_cfg['common']['user']
+	pwd = common_cfg['common']['password']
+	db = common_cfg['common']['database']
+	conn = Datasource(host,user,pwd,db).getConnection()
+	cursor = conn.cursor()
+	log.debug('构建报表数据')
+	cursor.execute(sql)
+	redisCli = redis.Redis( host=common_cfg['common']['store_host'],port=int(common_cfg['common']['store_port']),password=common_cfg['common']['store_password'] )
+	report_key = common_cfg['common']['report_key']
+	log.debug( '清除历史报表 key=%s' % report_key )
+	redisCli.delete(report_key)
+	for (channel,new_user,new_register,all_register,all_user,new_pv,all_pv,new_rate,all_rate) in cursor :
+		if not channel : channel = 0 
+		if not new_user : new_user = 0 
+		else : new_user = int(new_user)	
+		if not new_register : new_register = 0 
+		else : new_register = int(new_register)
+		if not all_register : all_register = 0 
+		else : all_register = int(all_register)
+		if not all_user	: all_user = 0 
+		else : all_user = int(all_user)
+		if not new_pv : new_pv = 0 
+		else : new_pv = int(new_pv)
+		if not all_pv : all_pv = 0 
+		else : all_pv = int(all_pv)
+		if not new_rate	: new_rate = 0.0 
+		else : new_rate = float(new_rate)
+		if not all_rate : all_rate = 0.0 
+		else : all_rate = float(all_rate)
+		k=('channel','new_user','new_register','all_register','all_user','new_pv','all_pv','new_rate','all_rate')
+		v=(channel,new_user,new_register,all_register,all_user,new_pv,all_pv,new_rate,all_rate)
+		vo = dict(zip(k,v))
+		j = json.dumps(vo)
+		redisCli.lpush(report_key,j)
+	log.debug('完成报表构建')
+	cursor.close()
+	conn.close()
 	return 'success'
 
 if __name__ == '__main__' :
-
-	common_cfg = mod_conf.load('access_log_analysis.ini')
 	LOG_LEVEL = lm.level['debug']
 	log = lm.LoggerFactory(common_cfg['common']['log_file'],'counter',LOG_LEVEL).getLog()
 	print common_cfg
@@ -121,3 +185,47 @@ if __name__ == '__main__' :
 	while True:
 		runner()
 		time.sleep(60)
+
+
+report_channel_counter_sql = """
+select  r.channel , 
+	r.new_user , 
+	r.new_register , 
+	r.all_register, 
+	r.all_user , 
+	r.new_pv , 
+	r.all_pv ,
+        (r.new_register/r.new_user) as new_rate/*今日注册率*/,
+        (all_register/all_user) as all_rate/*总注册率*/
+from  (
+ select
+    c0.channel/*渠道*/,
+    (select sum(c1.counter)
+      from biz_service_counter c1
+      where c1.channel=c0.channel
+	and c1.method='firstOpen'
+	and c1.cd='${cd}' ) as new_user/*今日新增*/,
+    (select sum(c2.counter)
+      from biz_service_counter c2
+      where c2.channel=c0.channel
+	and c2.method='register'
+	and c2.cd='2013-11-16' ) as new_register/*今日注册*/,
+    (select sum(c3.counter)
+      from biz_service_counter c3
+      where c3.channel=c0.channel
+	and c3.method='register' ) as all_register/*总注册数*/,
+    (select sum(c3.counter)
+      from biz_service_counter c3
+      where c3.channel=c0.channel
+	and c3.method='firstOpen' ) as all_user/*总用户数*/,
+    (select sum(c2.counter)
+      from biz_service_counter c2
+      where c2.channel=c0.channel
+	and c2.cd='${cd}' ) as new_pv/*今日PV*/,
+    (select sum(c2.counter)
+      from biz_service_counter c2
+      where c2.channel=c0.channel ) as all_pv/*总PV*/
+ from biz_service_counter c0
+ group by c0.channel
+)as r
+"""
