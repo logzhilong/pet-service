cb9825b415bce9bcc38e86af9b7c1047a8e5d844
cc14514 <cc14514@github.com>
2013-11-22 12:23:35 +0800

    增加审核开关

--- pet-wordfilter/src/main/java/com/momoplan/pet/framework/wordfilter/service/impl/WordFilterServiceImpl.java
+++ pet-wordfilter/src/main/java/com/momoplan/pet/framework/wordfilter/service/impl/WordFilterServiceImpl.java
@@ -14,6 +14,7 @@ import com.momoplan.pet.commons.domain.states.po.StatesUserStatesAudit;
 import com.momoplan.pet.commons.repository.bbs.NoteRepository;
 import com.momoplan.pet.commons.repository.bbs.NoteState;
 import com.momoplan.pet.commons.repository.states.StatesUserStatesRepository;
+import com.momoplan.pet.commons.spring.CommonConfig;
 import com.momoplan.pet.commons.wordfilter.WordFilterUtil;
 import com.momoplan.pet.commons.wordfilter.result.FilteredResult;
 import com.momoplan.pet.framework.wordfilter.service.WordFilterService;
@@ -27,18 +28,20 @@ public class WordFilterServiceImpl implements WordFilterService {
 	private MapperOnCache mapperOnCache = null;
 	private StatesUserStatesRepository statesUserStatesRepository = null;
 	private NoteRepository noteRepository = null;
+	private CommonConfig commonConfig = null;
 	
 	@Autowired
 	public WordFilterServiceImpl(
 			StatesUserStatesAuditMapper statesUserStatesAuditMapper,
 			MapperOnCache mapperOnCache,
 			StatesUserStatesRepository statesUserStatesRepository,
-			NoteRepository noteRepository) {
+			NoteRepository noteRepository, CommonConfig commonConfig) {
 		super();
 		this.statesUserStatesAuditMapper = statesUserStatesAuditMapper;
 		this.mapperOnCache = mapperOnCache;
 		this.statesUserStatesRepository = statesUserStatesRepository;
 		this.noteRepository = noteRepository;
+		this.commonConfig = commonConfig;
 	}
 
 	@Override
@@ -54,12 +57,21 @@ public class WordFilterServiceImpl implements WordFilterService {
 	}
 
 	private void userStatesHandler(String biz, String bid, String content) {
-		logger.debug("biz="+biz+" ; bid="+bid+" ; content="+content); 
+		String wordfilter = commonConfig.get("wordfilter","enable");
+		logger.debug("biz="+biz+" ; bid="+bid+" ; wordfilter="+wordfilter);
 		try {
 			// content_audit 审核任务
 			// user_states 动态
 			// state_type :0正常，1假动态，2置顶动态，3审核中，4未通过，5被举报
 			// report_times :举报次数
+			StatesUserStates statesUserStates = mapperOnCache.selectByPrimaryKey(StatesUserStates.class, bid);
+			if("disable".equalsIgnoreCase(wordfilter)){
+				logger.debug("后台已关闭审核--全部通过");
+				statesUserStates.setState("0");// 通过 TODO 放倒枚举类里
+				statesUserStatesRepository.updateSelective(statesUserStates);
+				return ;
+			}
+			logger.debug("开始自动审核...");
 			FilteredResult result = WordFilterUtil.filterText(content, '*');
 			String badWords = result.getBadWords();// 异常词
 			if (badWords != null && badWords.trim().length() > 0) {
@@ -71,15 +83,12 @@ public class WordFilterServiceImpl implements WordFilterService {
 				commonContentAudit.setContent(badWords);
 				statesUserStatesAuditMapper.insertSelective(commonContentAudit);
 				logger.debug(commonContentAudit.toString());
-				StatesUserStates statesUserStates = mapperOnCache.selectByPrimaryKey(StatesUserStates.class, bid);
 				statesUserStates.setState("4");// 未通过 TODO 放倒枚举类里
 				logger.debug(statesUserStates.toString());
 				statesUserStatesRepository.updateSelective(statesUserStates);
 			} else {
 				logger.debug("用户动态-内容审核-通过");
-				StatesUserStates statesUserStates = mapperOnCache.selectByPrimaryKey(StatesUserStates.class, bid);
 				statesUserStates.setState("0");// 通过 TODO 放倒枚举类里
-				logger.debug(statesUserStates.toString());
 				statesUserStatesRepository.updateSelective(statesUserStates);
 			}
 		} catch (Exception e) {
@@ -88,16 +97,25 @@ public class WordFilterServiceImpl implements WordFilterService {
 	}
 
 	private void bbsNoteHandler(String biz, String bid, String content) {
-		logger.debug("biz="+biz+" ; bid="+bid+" ; content="+content); 
+		String wordfilter = commonConfig.get("wordfilter","enable");
+		logger.debug("biz="+biz+" ; bid="+bid+" ; wordfilter="+wordfilter);
 		try {
 			// content_audit 审核任务
 			// bbs_note 帖子
 			// state : NoteState 枚举
 			// report_times :举报次数
+			Note note = mapperOnCache.selectByPrimaryKey(Note.class, bid);
+			if("disable".equalsIgnoreCase(wordfilter)){
+				logger.debug("后台已关闭审核--全部通过");
+				note.setState(NoteState.PASS.getCode());//审核通过
+				noteRepository.insertOrUpdateSelective(note, NoteState.PASS);
+				return;
+			}
+			logger.debug("开始自动审核...");
 			FilteredResult result = WordFilterUtil.filterText(content, '*');
 			String badWords = result.getBadWords();// 异常词
 			if (badWords != null && badWords.trim().length() > 0) {
-				logger.debug("用户动态-内容审核-不通过 badWords=" + badWords);
+				logger.debug("内容审核-不通过 badWords=" + badWords);
 				StatesUserStatesAudit commonContentAudit = new StatesUserStatesAudit();
 				commonContentAudit.setId(IDCreater.uuid());
 				commonContentAudit.setBiz(biz);
@@ -105,16 +123,13 @@ public class WordFilterServiceImpl implements WordFilterService {
 				commonContentAudit.setContent(badWords);
 				statesUserStatesAuditMapper.insertSelective(commonContentAudit);
 				logger.debug(commonContentAudit.toString());
-				Note note = mapperOnCache.selectByPrimaryKey(Note.class, bid);
 				note.setState(NoteState.REJECT.getCode());//审核拒绝
 				logger.debug(note.toString());
 				noteRepository.insertOrUpdateSelective(note, NoteState.REJECT);
-			} else {
-				logger.debug("用户动态-内容审核-通过");
-				Note note = mapperOnCache.selectByPrimaryKey(Note.class, bid);
+			}else{
+				logger.debug("审核通过");
 				note.setState(NoteState.PASS.getCode());//审核通过
-				logger.debug(note.toString());
-				noteRepository.insertOrUpdateSelective(note, NoteState.PASS);		
+				noteRepository.insertOrUpdateSelective(note, NoteState.PASS);	
 			}
 		} catch (Exception e) {
 			logger.error("userStatesHandler",e);
