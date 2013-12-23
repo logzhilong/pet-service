b83a739b410f28156302dca7ea09e3cda4521ea0
cc14514 <cc14514@github.com>
2013-12-23 11:29:56 +0800

    总用户数修正

--- utils/access_log_analysis.py
+++ utils/access_log_analysis.py
@@ -34,7 +34,7 @@ from  (
       from biz_service_counter c3
       where c3.channel=c0.channel
 	and c3.method='register' ) as all_register/*总注册数*/,
-    (select count(0) from biz_imei ) as all_user/*总用户数*/,
+    (select count(0) from biz_imei c4 where c4.channel=c0.channel ) as all_user/*总用户数*/,
     (select sum(c5.counter)
       from biz_service_counter c5
       where c5.channel=c0.channel
